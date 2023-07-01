98ef9e70512f15284e73d1857e6665846b8af7c4
juancristobalgd1 <65052633+juancristobalgd1@users.noreply.github.com>
2023-07-01 14:18:43 +0200

    Update Liveaxm.php

--- app/Config/Liveaxm.php
+++ app/Config/Liveaxm.php
@@ -7,27 +7,27 @@ return [
     | Class Namespace
     |--------------------------------------------------------------------------
     |
-    | This value sets the root namespace for Liveaxm component classes in
+    | This value sets the root namespace for raxm component classes in
     | your application. This value affects component auto-discovery and
-    | any Liveaxm file helper commands, like `artisan make:liveaxm`.
+    | any raxm file helper commands, like `artisan make:raxm`.
     |
-    | After changing this item, run: `php artisan liveaxm:discover`.
+    | After changing this item, run: `php artisan raxm:discover`.
     |
     */
 
-    'class_namespace' => 'App\\Http\\Liveaxm',
+    'class_namespace' => 'App\\Http\\Raxm',
 
     /*
     |--------------------------------------------------------------------------
     | View Path
     |--------------------------------------------------------------------------
     |
-    | This value sets the path for Liveaxm component views. This affects
-    | file manipulation helper commands like `artisan make:liveaxm`.
+    | This value sets the path for raxm component views. This affects
+    | file manipulation helper commands like `artisan make:raxm`.
     |
     */
 
-    'view_path' => AXM_PATH . '/views/liveaxm/',
+    'view_path' => APP_PATH . '/views/raxm/',
 
     /*
     |--------------------------------------------------------------------------
@@ -43,42 +43,42 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Liveaxm Assets URL
+    | raxm Assets URL
     |--------------------------------------------------------------------------
     |
-    | This value sets the path to Liveaxm JavaScript assets, for cases where
-    | your app's domain root is not the correct path. By default, Liveaxm
+    | This value sets the path to raxm JavaScript assets, for cases where
+    | your app's domain root is not the correct path. By default, raxm
     | will load its JavaScript assets from the app's "relative root".
     |
     | Examples: "/assets", "myurl.com/app".
     |
     */
 
-    'asset_url' => 'assets/js/ax/dist',
+    'asset_url' =>  $_SERVER['HTTP_HOST'] . '/vendor/axm/raxm/dist/',
 
     /*
     |--------------------------------------------------------------------------
-    | Liveaxm File Name
+    | raxm File Name
     |--------------------------------------------------------------------------
     |
-    | This value sets the filename to Liveaxm JavaScript assets, for cases where
-    | your app's domain root is not the correct path. By default, Liveaxm
+    | This value sets the filename to raxm JavaScript assets, for cases where
+    | your app's domain root is not the correct path. By default, raxm
     | will load its JavaScript assets from the app's "file name".
     |
-    | Examples: "/cdn.js".
+    | Examples: "cdn.js".
     |
     */
 
-    'fileName' => '/cdn.js',
+    'fileName' => 'cdn.js',
 
 
     /*
     |--------------------------------------------------------------------------
-    | Liveaxm App URL
+    | raxm App URL
     |--------------------------------------------------------------------------
     |
-    | This value should be used if liveaxm assets are served from CDN.
-    | Liveaxm will communicate with an app through this url.
+    | This value should be used if raxm assets are served from CDN.
+    | raxm will communicate with an app through this url.
     |
     | Examples: "https://my-app.com", "myurl.com/app".
     |
@@ -88,12 +88,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Liveaxm Endpoint Middleware Group
+    | raxm Endpoint Middleware Group
     |--------------------------------------------------------------------------
     |
     | This value sets the middleware group that will be applied to the main
-    | Liveaxm "message" endpoint (the endpoint that gets hit everytime
-    | a Liveaxm component updates). It is set to "web" by default.
+    | raxm "message" endpoint (the endpoint that gets hit everytime
+    | a raxm component updates). It is set to "web" by default.
     |
     */
 
@@ -101,10 +101,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Liveaxm Temporary File Uploads Endpoint Configuration
+    | raxm Temporary File Uploads Endpoint Configuration
     |--------------------------------------------------------------------------
     |
-    | Liveaxm handles file uploads by storing uploads in a temporary directory
+    | raxm handles file uploads by storing uploads in a temporary directory
     | before the file is validated and stored permanently. All file uploads
     | are directed to a global endpoint for temporary storage. The config
     | items below are used for customizing the way the endpoint works.
@@ -112,16 +112,21 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              Default: 'default'
-        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      Default  'liveaxm-tmp'
-        'middleware' => null,  // Example: 'throttle:5,1'             Default: 'throttle:60,1'
-        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs.
-            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-            'mov', 'avi', 'wmv', 'mp3', 'm4a',
-            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+        'disk'  => null,       # Example: 'local', 's3'              Default: 'default'
+        'rules' => null,       # Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory'  => null,  # Example: 'tmp'                      Default  'raxm-tmp'
+        'middleware' => null,  # Example: 'throttle:5,1'             Default: 'throttle:60,1'
+        'preview_mimes' => [   # Supported file types for temporary pre-signed file URLs.
+            'png', 'gif',
+            'bmp', 'svg',
+            'wav', 'mp4',
+            'mov', 'avi',
+            'wmv', 'mp3',
+            'm4a', 'wma',
+            'jpg', 'jpeg',
+            'mpga', 'webp',
         ],
-        'max_upload_time' => 5, // Max duration (in minutes) before an upload gets invalidated.
+        'max_upload_time' => 5, # Max duration (in minutes) before an upload gets invalidated.
     ],
 
     /*
@@ -129,12 +134,12 @@ return [
     | Manifest File Path
     |--------------------------------------------------------------------------
     |
-    | This value sets the path to the Liveaxm manifest file.
+    | This value sets the path to the raxm manifest file.
     | The default should work for most cases (which is
-    | "<app_root>/bootstrap/cache/liveaxm-components.php"), but for specific
+    | "<app_root>/bootstrap/cache/raxm-components.php"), but for specific
     | cases like when hosting on Laravel Vapor, it could be set to a different value.
     |
-    | Example: for Laravel Vapor, it would be "/tmp/storage/bootstrap/cache/liveaxm-components.php".
+    | Example: for Laravel Vapor, it would be "/tmp/storage/bootstrap/cache/raxm-components.php".
     |
     */
 
@@ -146,7 +151,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | This value determines whether the back button cache will be used on pages
-    | that contain Liveaxm. By disabling back button cache, it ensures that
+    | that contain raxm. By disabling back button cache, it ensures that
     | the back button shows the correct state of components, instead of
     | potentially stale, cached data.
     |
@@ -161,7 +166,7 @@ return [
     | Render On Redirect
     |--------------------------------------------------------------------------
     |
-    | This value determines whether Liveaxm will render before it's redirected
+    | This value determines whether raxm will render before it's redirected
     | or not. Setting it to "false" (default) will mean the render method is
     | skipped when redirecting. And "true" will mean the render method is
     | run before redirecting. Browsers bfcache can store a potentially
@@ -172,3 +177,4 @@ return [
     'render_on_redirect' => false,
 
 ];
+
